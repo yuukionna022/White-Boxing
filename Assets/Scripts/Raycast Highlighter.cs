43e0f6ececd69c79ad404b3e329531e090d7e606
Alan Huang <alanhuang777@gmail.com>
2024-10-25 18:08:21 -0700

    Auto stash before rebase of "main" onto "origin/main"

---
+++ Assets/Scripts/Raycast Highlighter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHighlighter : MonoBehaviour
+{
+    public float distanceToSee;
+    public string ObjectName;
+    private Color highlightColor;
+    Material originalMaterial, tempMaterial;
+    Renderer rend = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highlightColor = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Renderer currRend;
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hitInfo, distanceToSee))
+        {
+            currRend = hitInfo.collider.gameObject.GetComponent<Renderer>();
+
+            if (currRend == rend)
+                return;
+
+            if (currRend && currRend != rend)
+            {
+                if (rend)
+                {
+                    rend.sharedMaterial = originalMaterial;
+                }
+
+            }
+
+            if (currRend)
+                rend = currRend;
+            else
+                return;
+
+            originalMaterial = rend.sharedMaterial;
+
+            tempMaterial = new Material(originalMaterial);
+            rend.material = tempMaterial;
+            rend.material.color = highlightColor;
+        }
+        else
+        {
+            if (rend)
+            {
+                rend.sharedMaterial = originalMaterial;
+                rend = null;
+            }
+        }
+    }
+}
