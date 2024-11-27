55258132d8f30c282595a201416159b816d22a6c
Yuki Cai <157555294+yuukionna022@users.noreply.github.com>
2024-11-26 21:44:24 -0800

    eyeballs

---
+++ Assets/Eyeball.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyeball : MonoBehaviour
+{
+    private GameObject player;
+    private float max, min, fixedx, fixedz;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player Collider");
+        max = 296.49f;
+        min = 244.36f;
+        fixedx = transform.localEulerAngles.x;
+        fixedz = transform.localEulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null && (player.transform.position - transform.position).magnitude < 6f)
+        {
+            transform.LookAt(player.transform.position);
+        }
+
+        Debug.Log(transform.localEulerAngles);
+
+        if (transform.localEulerAngles.y < min)
+        {
+            transform.localEulerAngles = new Vector3(fixedx, min, fixedz);
+        }
+        else if (transform.localEulerAngles.y > max)
+        {
+            transform.localEulerAngles = new Vector3(fixedx, max, fixedz);
+        }
+
+        if (transform.localEulerAngles.x < fixedx || transform.localEulerAngles.x > fixedx)
+        {
+            transform.localEulerAngles = new Vector3(fixedx, transform.localEulerAngles.y, fixedz);
+        }
+       
+
+
+    }
+}
