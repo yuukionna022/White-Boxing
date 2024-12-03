7630188fc769f88ae35fb0c35fcfdc6b3df5a4e9
Yuki Cai <157555294+yuukionna022@users.noreply.github.com>
2024-12-02 18:41:32 -0800

    scuffed ui

--- Assets/TeleportPlayer.cs
+++ Assets/TeleportPlayer.cs
@@ -11,6 +11,7 @@ public class TeleportPlayer : MonoBehaviour
     void Start()
     {
         //Vector3(25.5805054, 7.94321632, -3.22506523)
+        //UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":60.38949203491211,"y":0.7750000953674316,"z":2.495065689086914},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} }
     }
 
     // Update is called once per frame
