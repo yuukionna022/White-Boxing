0ef2a0d378270dc981a8dec0c3ba7e1bd0417476
Alan Huang <alanhuang777@gmail.com>
2024-12-04 01:56:51 -0800

    Book Teleport

--- Assets/Scripts/TeleportPlayer.cs
+++ Assets/Scripts/TeleportPlayer.cs
@@ -6,12 +6,16 @@ public class TeleportPlayer : MonoBehaviour
 {
     public GameObject player;
     public GameObject storePosition;
-    public Vector3 destination;
+    //public Vector3 destination;
+    public GameObject destinationObject;
     // Start is called before the first frame update
     void Start()
     {
-        //Vector3(25.5805054, 7.94321632, -3.22506523)
-        //UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":60.38949203491211,"y":0.7750000953674316,"z":2.495065689086914},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} }
+        //UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":-33.35050582885742,"y":0.7750000953674316,"z":-13.514934539794922},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} }
+        //-33.35050582885742,"y":0.7750000953674316,"z":-13.514934539794922
+        //UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":103.74949645996094,"y":0.7750000953674316,"z":23.355064392089845},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} }
+        
+
     }
 
     // Update is called once per frame
@@ -22,10 +26,11 @@ public class TeleportPlayer : MonoBehaviour
 
     public void teleportPlayer()
     {
+        Debug.Log("teleport Player");
         //store the player position
         storePosition.transform.position = player.transform.position;
         //teleport the player
-        player.transform.position = destination;
+        player.transform.position = destinationObject.transform.position;
     }
     public void teleportPlayerBack()
     {
