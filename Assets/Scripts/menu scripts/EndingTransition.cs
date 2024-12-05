bc4366403c152064c2dd8365f814a3e026873498
Yuki Cai <157555294+yuukionna022@users.noreply.github.com>
2024-12-05 12:02:19 -0800

    transition attempt

---
+++ Assets/Scripts/menu scripts/EndingTransition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingTransition : MonoBehaviour
+{
+
+    public TeleportPlayer teleport;
+    public TeleportTransition transition;
+    public Collider player;
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other == player)
+        {
+            transition.fadeTransition();
+            teleport.teleportPlayerBack();
+
+        }
+    }
+}
