c93019c01676b60f9219eb535479ac21a5fb508c
Yuki Cai <157555294+yuukionna022@users.noreply.github.com>
2024-12-06 18:10:33 -0800

    drop menu book

--- Assets/Scripts/menu scripts/DropMenuBook.cs
+++ Assets/Scripts/menu scripts/DropMenuBook.cs
@@ -9,11 +9,13 @@ public class DropMenuBook : MonoBehaviour
     public Collider roomCollider;
     public PuzzleConditions conditions;
     public TeleportPlayer teleporter;
-    private Vector3 orig;
+    private Vector3 one, two, three;
 
     void Start()
     {
-        orig = gameObject.transform.position; 
+       one = new Vector3(55.80258560180664f, 0.5442066192626953f, 4.880788803100586f);
+       two = new Vector3(63.58655548095703f, 0.36991643905639653f, -3.430189609527588f);
+       three = new Vector3(57.515750885009769f, 0.05655479431152344f, 8.976882934570313f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,7 +39,18 @@ public class DropMenuBook : MonoBehaviour
     IEnumerator Pause()
     {
         gameObject.GetComponent<XRGrabInteractable>().enabled = false;
-        gameObject.transform.position = orig;
+        if (conditions.getState() == 0)
+        {
+            gameObject.transform.position = one;
+        }
+        else if (conditions.getState() == 1)
+        {
+            gameObject.transform.position = two;
+        }
+        else 
+        {
+            gameObject.transform.position = three;
+        }
         yield return new WaitForSeconds(0.02f);
         gameObject.GetComponent<XRGrabInteractable>().enabled = true;
     }
